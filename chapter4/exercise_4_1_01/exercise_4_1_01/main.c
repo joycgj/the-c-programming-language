6623e34b4603efee3a047a9c78e349a43678e43e
chengaojie <chengaojie@diditaxi.com.cn>
2016-09-07 16:52:08 +0800

    exercise 4.1 01

---
+++ chapter4/exercise_4_1_01/exercise_4_1_01/main.c
@@ -0,0 +1,64 @@
+//
+//  main.c
+//  exercise_4_1_01
+//
+//  Created by chengaojie on 16/9/7.
+//  Copyright © 2016年 chengaojie. All rights reserved.
+//
+
+#include <stdio.h>
+#define MAXLINE 1000    /* maxinum input line length */
+
+int ngetline(char line[], int max);
+int nstrrightmostindex(char s[], char t[]);
+
+char pattern[] = "ould";    /* pattern to search for */
+
+/*  find all lines matching pattern */
+int main(int argc, const char * argv[]) {
+    char line[MAXLINE];
+    int found  = 0;
+    
+    while (ngetline(line, MAXLINE) > 0) {
+        if (nstrrightmostindex(line, pattern) >= 0) {
+            found++;
+        }
+    }
+    
+    printf("%d line contains %s\n", found, pattern);
+    return found;
+}
+
+/* ngetline: get line into s, return length */
+int ngetline(char line[], int max) {
+    int c, i;
+    
+    c = 0;
+    i = 0;
+    while (--max > 0 && (c=getchar()) != EOF && c != '\n') {
+        line[i++] = c;
+    }
+    
+    if (c == '\n') {
+        line[i++] = c;
+    }
+    line[i] = '\0';
+    return i;
+}
+
+/* nstrrightmostindex: returns rightmost index of t in s, -1 if none */
+int nstrrightmostindex(char s[], char t[]) {
+    int i, j, k, pos;
+    
+    pos = -1;
+    
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) {
+            ;
+        }
+        if (k > 0 && t[k] == '\0') {
+            pos = i;
+        }
+    }
+    return pos;
+}
